d556708996b59e5558592f8ee608a0bfec358573
ArekG <arekgajda1996@gmail.com>
2020-05-04 20:37:03 +0200

    dodanie nagrania

---
+++ src/main/java/app/listener/AerospikeWriteListener.java
@@ -0,0 +1,32 @@
+package app.listener;
+
+import com.aerospike.client.*;
+import com.aerospike.client.listener.WriteListener;
+import com.aerospike.client.policy.WritePolicy;
+import app.common.PriceCalculatorService;
+import app.travelagency.Travel;
+
+public class AerospikeWriteListener implements WriteListener {
+    private final AerospikeClient client;
+    private final PriceCalculatorService priceCalculatorService = new PriceCalculatorService();
+
+    public AerospikeWriteListener(AerospikeClient client) {
+        this.client = client;
+    }
+
+    @Override
+    public void onSuccess(Key key) {
+        Travel travel = (Travel) client.get(null, key).bins.get("travelobject");
+        travel.setPrice(priceCalculatorService.calculatePrice(travel));
+        Bin bin = new Bin("travelobject", Value.getAsBlob(travel));
+        WritePolicy writePolicy = new WritePolicy();
+        writePolicy.sendKey = true;
+        client.put(writePolicy, key, bin);
+
+    }
+
+    @Override
+    public void onFailure(AerospikeException e) {
+
+    }
+}
